e89359c567cc70a926f903a044c0029b8bc6c953
gagan <gagan@Neha>
2022-11-15 09:26:24 +0530

    COMMITTED

---
+++ src/main/java/mavenb577/cseb577/App.java
@@ -0,0 +1,13 @@
+package mavenb577.cseb577;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello 20MH1A0577 !" );
+    }
+}
